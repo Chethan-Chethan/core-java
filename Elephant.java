b5fdd96c76252e1d1fb9e0efc68b7430fb65e774
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:15:07 +0530

    commit

---
+++ Elephant.java
@@ -0,0 +1,38 @@
+class Elephant {
+	private String name;
+	private int age;
+	private int weight;
+	
+	public Elephant() {
+		this.name = "Abimanyu";
+		this.age = 55;
+		this.weight = 506;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	
+	
+	
+}
