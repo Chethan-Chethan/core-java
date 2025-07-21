1e4ff0b24e0cdb0d6125d6685ea519a6a49d9a06
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:28:20 +0530

    fav

---
+++ FavoriteActor.java
@@ -0,0 +1,13 @@
+class FavoriteActor {
+	public static void main(String [] args) {
+		String details=getDetails();
+		System.out.println(details);
+	}
+	
+	public static String getDetails() {
+		String actor = "Yash";
+		String movie = "KGF";
+		String color = "Black";
+		
+	return "My favorite actor is " + actor + ", favorite movie is " + movie + ", and favorite color is " + color + ".";}
+}
