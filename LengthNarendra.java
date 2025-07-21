015ff0f0f9c08aaed03459ba8d780d4060cefe95
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:46:34 +0530

    len

---
+++ LengthNarendra.java
@@ -0,0 +1,9 @@
+class LengthNarendra {
+	public static void main(String [] args) {
+		int nameLength = getLength();
+		System.out.println("Full Length of Name " + nameLength);
+	}
+	public static int getLength() {
+		String name = "Narendra";
+	return name.length();}
+}
