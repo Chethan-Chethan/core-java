81be8810fd4cce67bd24e66123c87dbc803daaa7
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:22:07 +0530

    fan

---
+++ Fan.java
@@ -0,0 +1,8 @@
+class Fan {
+	static String brand = getFanBrand();
+	public static void main(String [] args) {
+		System.out.println("Fan Brand is = " + brand);
+	}
+	public static String getFanBrand() {
+	return "Usha";}
+}
