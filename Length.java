cdfd4b27dbe377aedbac1bbf3f64eadd3f6e3b35
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:44:36 +0530

    length

---
+++ Length.java
@@ -0,0 +1,10 @@
+class Length {
+	public static void main(String [] args) {
+		int nameLength = getLength();
+		System.out.println("Full Length = " + nameLength);
+	}
+	public static int getLength() {
+	String name = "Chethan G";
+	return name.length();}
+	
+}
