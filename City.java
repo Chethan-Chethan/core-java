081bab4e3398433869b215cf0cf18264f2f77e01
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 21:58:47 +0530

    city

---
+++ City.java
@@ -0,0 +1,9 @@
+class City {
+	static String city = getCityName();
+	
+	public static void main(String [] args) {
+	System.out.println("City Name = " + city);}
+	
+	public static String getCityName() {
+	return "Bengaluru";}
+}
