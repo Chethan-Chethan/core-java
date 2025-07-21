7c25d84e53dfdec42cb67dafe297b7f43f491e1b
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 11:41:33 +0530

    brand

---
+++ BikeBrand.java
@@ -0,0 +1,9 @@
+class BikeBrand {
+	public static void main(String [] args) {
+		String bikeBrand = getBikeBrand();
+		System.out.println("My Bike Brand is = " + bikeBrand);
+	}
+	
+	public static String getBikeBrand() {
+	return "Yamaha";}
+}
