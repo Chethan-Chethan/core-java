4f62298509684511b0de5b51476fbdff0cdf2f74
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:23:41 +0530

    fav

---
+++ FavActor.java
@@ -0,0 +1,9 @@
+class FavActor {
+	
+	public static void main(String [] args) {
+		String myFavActor = yash();
+	System.out.println("My FavActor = " + myFavActor);}
+	
+	public static String yash() {
+	return "yash";}
+}
