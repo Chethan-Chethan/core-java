8da54f1b97be39fdb3c1d887c5ce6b09651323ba
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:30:24 +0530

    file

---
+++ File.java
@@ -0,0 +1,11 @@
+class File
+{
+public static void main(String [ ] args)
+
+{
+System.out.println("Welcom to Java");
+System.out.println("Great one");
+System.out.println("Hello" + 2);
+System.out.println("Hello" + 4);
+}
+}
