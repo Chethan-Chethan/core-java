6f2e8c08d361b140a4019d2312f98c4f5a492405
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:13:21 +0530

    double

---
+++ DoublePrice.java
@@ -0,0 +1,9 @@
+class DoublePrice {
+	public static void main(String [] args) {
+		double totalMobilePrice = getMobilePrice();
+		System.out.println("Total cost of two Phones = $ " + totalMobilePrice);
+	}
+	
+	public static double getMobilePrice() {
+	return 18999*2;}
+}
