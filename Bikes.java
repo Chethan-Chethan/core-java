ea7694b4c9cf3c7f09bff391613ee445d0737e71
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 11:46:09 +0530

    bikes

---
+++ Bikes.java
@@ -0,0 +1,34 @@
+class Bikes {
+	
+	private String name;
+	private double cost;
+	private int speed;
+	
+	public void setName(String bikeName) {
+		name = bikeName;
+	}
+	
+	public void setCost(double bikeCost) {
+		cost = bikeCost;
+	}
+	
+	public void setSpeed(int bikeSpeed) {
+		speed = bikeSpeed;
+	}
+	
+	public String getName() {
+		return name;
+	}
+		
+	public double getCost() {
+		return cost;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+}
+
+	
+
+		
