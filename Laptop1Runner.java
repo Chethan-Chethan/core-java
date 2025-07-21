a847cefed51f54b4f8675e5fe604968e6dd3f477
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:37:47 +0530

    laptop

---
+++ Laptop1Runner.java
@@ -0,0 +1,9 @@
+class Laptop1Runner {
+	public static void main(String [] args) {
+		
+	    Laptop1 l1 = new Laptop1();
+	    l1.setMethod("HP", 55000f, 8, 512, "i5");
+	    System.out.println(l1.getMethod());
+	}
+}
+	
