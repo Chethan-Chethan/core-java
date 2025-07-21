86676b93ecae3a3213d7069d9a4d487631e67981
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:06:33 +0530

    details

---
+++ Details.java
@@ -0,0 +1,18 @@
+class Details
+{
+public static void main(String [ ] args)
+{
+String name = "Chethan";
+int age = 28;
+double height = 175;
+char initial = 'C';
+boolean isLearningJava = true;
+
+System.out.println("Name: " + name);
+System.out.println("Age: " + age);
+System.out.println("Height: " + height);
+System.out.println("Initial: " + initial);
+System.out.println("Learning Java: " + isLearningJava);
+
+}
+}
