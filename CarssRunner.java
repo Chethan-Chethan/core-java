e074a41e74dde725799c283e119902a837fe65f0
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 21:44:36 +0530

    cars

---
+++ CarssRunner.java
@@ -0,0 +1,11 @@
+class CarssRunner {
+	 public static void main(String [] args) {
+		Carss car = new Carss();
+		 System.out.println(car.getBrand());
+		 car.setBrand("Range rover");
+		 String brand = car.getBrand();
+		 System.out.println(brand);
+	 }
+ }
+ 
+		 
