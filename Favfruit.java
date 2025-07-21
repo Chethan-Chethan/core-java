e1aaabd9657199c81a53960702c6190ade5cc2c9
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:26:51 +0530

    fav fruit

---
+++ Favfruit.java
@@ -0,0 +1,8 @@
+class Favfruit {
+	static String fruit = getFavFruit();
+	public static void main(String [] args) {
+	System.out.println("My Fav Fruit is = " + fruit);}
+	
+	public static String getFavFruit() {
+	return "Mango";}
+}
