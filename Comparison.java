acb7e55051ebcc5e32a7b6c58fe7c35341e533d1
Chethan-chethan <chethang.xworkz@outlook>
2025-07-21 22:04:04 +0530

    comparison

---
+++ Comparison.java
@@ -0,0 +1,13 @@
+class Comparison {
+  public static void main(String [ ] args){
+  int x = 25;
+  int y = 40;
+  
+  System.out.println("Is x less than y? " + (x < y));
+  System.out.println("Is x equal to y? " + (x==y));
+  System.out.println("Is x not equal to y? " + (x!=y));
+  System.out.println("Is x greater than or equal to y? " + (x >= y));
+  
+  
+  }
+}
