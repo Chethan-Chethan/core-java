ec0ec20e83f9dcf6b70431062a72870f4b8c5f57
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 13:21:30 +0530

    voting

---
+++ VotingEligibility.java
@@ -0,0 +1,11 @@
+class VotingEligibility {
+  public static void main(String [] args) {
+	  int age = 22;
+	  boolean hasID = true;
+	  
+	  if (age >= 18 && hasID) {
+	  System.out.println("Eligible for voting");}
+	  else {
+	  System.out.println("Not eligible for Voting");}
+  }
+}
