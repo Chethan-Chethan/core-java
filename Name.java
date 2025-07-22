c0176729654267007ddaa2560d3b582636104dc8
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:22:38 +0530

    name

---
+++ Name.java
@@ -0,0 +1,8 @@
+class Name {
+	public static void main(String [] args) {
+		String myName = chethan();
+		System.out.println("My Name is = " + myName);
+	}
+	public static String chethan() {
+	return "Chethan G";}
+}
