f40411adcae9b6afb767ec9b11a84133a4855cba
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:10:50 +0530

    mobile

---
+++ Mobile.java
@@ -0,0 +1,10 @@
+class Mobile {
+	
+	public static void main(String [] args) {
+		double result = tripleMobilePrice();
+	System.out.println("Mobile Price is = "+ result); }
+	
+	public static double tripleMobilePrice () {
+	return 500000*3;}
+}
+			
