59064149aef3f89f0921cf338b8da38dd3bdd3ba
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:38:06 +0530

    pra

---
+++ Practice2.java
@@ -0,0 +1,17 @@
+class Practice2
+{
+public static void main(String [ ] args)
+{
+String name = "Chethan";
+long rollNumber = 1001500406L;
+double percentage = 78.89;
+char grade = 'A';
+boolean passed = true;
+
+System.out.println("Student Name: " + name);
+System.out.println("Roll Number: " + rollNumber);
+System.out.println("Percentage: " + percentage);
+System.out.println("Grade: " + grade);
+System.out.println("Passed: " + passed);
+}
+}
