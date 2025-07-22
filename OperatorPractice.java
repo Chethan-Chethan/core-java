b5a4b854f27d1954521d504e43f7aaf62b6ec6f5
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:26:04 +0530

    operator

---
+++ OperatorPractice.java
@@ -0,0 +1,22 @@
+class OperatorPractice {
+  public static void main(String [ ] args) {
+  int a = 15;
+  int b = 4;
+  
+  // Arithmetic Operations
+  System.out.println("Application (a + b): " + (a + b));
+  System.out.println("Subtraction (a - b): " + (a - b));
+  System.out.println("Multiplication (a * b): " + (a * b));
+  System.out.println("Division (a / b): " + (a / b));
+  System.out.println("Remainder (a % b): " + (a % b));
+  
+  // Comparison Operations
+  System.out.println("Is a greater than b? " + (a > b));
+  System.out.println("Is a equal to b? " + (a == b));
+  System.out.println("Is a greater than b? " + (a > b));
+  System.out.println("Is a divisible by b? " + (a % b == 0));
+  }
+ 
+  }
+  
+  
