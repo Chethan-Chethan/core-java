fb97e9e1218e307fe372cd399cb5f1f8b1700ab1
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:21:12 +0530

    first

---
+++ MyFirstProgram.java
@@ -0,0 +1,21 @@
+public class MyFirstProgram {
+
+public static void main(String [ ] args)
+{
+System.out.println("Namaskara, Nanna hesaru Chethan !");
+System.out.println("Namaskara! Nanna hesaru Chethan !");
+System.out.println("Java oduva kalege Swagatha !");
+System.out.println("Naanu Chethan.");
+System.out.println("Naanu Java Pro aagbeku");
+// This is my personal details
+System.out.println("My Name is Chethan");
+System.out.println("Iam from Karnataka");
+System.out.println("I want to becom a java Pro");
+System.out.print("no rest");
+System.out.println(" only hard work");
+System.out.print(" I Love Java. ");
+System.out.print("Iam Practicing Daily!");
+}
+
+}
+
