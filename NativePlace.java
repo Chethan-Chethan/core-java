d8dbe85c8deb569e6477a5c15b3023c73d9b8db5
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:23:46 +0530

    native

---
+++ NativePlace.java
@@ -0,0 +1,8 @@
+class NativePlace {
+	public static void main(String [] args) {
+		String nativePlace = batlahalli();
+	System.out.println("My Native Place is = " + nativePlace);}
+	
+	public static String batlahalli() {
+	return "batlahalli";}
+}
