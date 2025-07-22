8e08afbe9ac40cbf3f46a8b075dd5d67d86036da
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:39:25 +0530

    pratice 4

---
+++ Practice4.java
@@ -0,0 +1,23 @@
+class Practice3
+{
+public static void main(String [ ] args)
+{
+String brand = "Pulsur";
+int modelYear = 2018;
+double millage = 35;
+char initial = 'P';
+boolean electric = true;
+String day = "Monday";
+int date = 11-6-2006;
+double temp = 27.8;
+
+System.out.println("Brand: " + brand);
+System.out.println("Model Year: " + modelYear);
+System.out.println("Millage: " + millage);
+System.out.println("Initial: " + initial);
+System.out.println("Electric: " + electric);
+System.out.println("Day: " + day);
+System.out.println("Date: " + date);
+System.out.println("Temperature: " + temp);
+}
+}
