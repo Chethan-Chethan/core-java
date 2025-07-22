d76bedcd6c7ce0ff3800d2272b4d847e353b431d
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:24:59 +0530

    number

---
+++ Number.java
@@ -0,0 +1,11 @@
+class Number {
+
+
+
+public static void main(String [] args) {
+	int result = square();
+System.out.println("Square is =" + result); }
+
+public static int square() {
+return 5*5;}
+}
