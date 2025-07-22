3d9b69fe4d61e5e5444296471ec409b388f541ce
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 13:03:38 +0530

    tool

---
+++ ToolPrice.java
@@ -0,0 +1,9 @@
+class ToolPrice {
+	public static void main(String [] args ) {
+		float toolPrice = getToolPrice();
+	System.out.println("Tool Price = " + toolPrice);
+	}
+	public static float getToolPrice() {
+	return 3*27.8f;}
+}
+	
