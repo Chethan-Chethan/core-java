c624af5c59dbedbb592cc8d899d67aebe57dfc30
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:42:26 +0530

    revmulti

---
+++ ReverseMultiplication.java
@@ -0,0 +1,12 @@
+class ReverseMultiplication {
+	public static void main(String [] args) {
+		int number = 3;
+		//for (int i = 10; i >= 1; i--) {
+			System.out.println("Multiplication of Table" + number);
+			for (int i = 10; i >= 1; i--) {
+			System.out.println(number + "x" + i + "=" + (number * i));
+		}
+		    System.out.println();
+	}
+}
+			
