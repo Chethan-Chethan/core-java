25602bc406116ca9f2c04db49329b1681505d205
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:47:35 +0530

    school

---
+++ School.java
@@ -0,0 +1,18 @@
+class School {
+	static String schoolName = getSchoolName();
+	static int strength = getStrength();
+	static float distance = getDistance();
+
+
+public static void main(String [] args) {
+	System.out.println("My School Name = " + schoolName);
+	System.out.println("School Strength = " + strength);
+	System.out.println("School distance from Home = " + distance);
+	
+}
+
+
+
+}
+
+	
