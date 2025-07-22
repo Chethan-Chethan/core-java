c7f9fa6a7b3c31d8decc8acdeb9edd13084b2d9d
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:36:53 +0530

    practice

---
+++ Practice1.java
@@ -0,0 +1,16 @@
+class Practice1
+{
+public static void main(String [ ] args)
+{
+
+String favcolor = "Blue";
+long mobileNumber = 8951191490L;
+int age = 20;
+boolean isEligibleToVote = age >= 18;
+
+System.out.println("My favourite color is: " + favcolor);
+System.out.println("My mobile number is: "+ mobileNumber);
+System.out.println("Eligible to Vote: " + isEligibleToVote);
+
+}
+}
