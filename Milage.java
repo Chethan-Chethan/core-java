5a6b55d02b0714fc48a7e05a37858d09988f7442
Chethan-chethan <chethang.xworkz@outlook>
2025-07-22 12:09:51 +0530

    milage

---
+++ Milage.java
@@ -0,0 +1,7 @@
+class Milage {
+	public static void main(String [] args) {
+		float milage = getMilage();
+	System.out.println("Milage = " + milage);}
+	public static float getMilage() {
+	return 36f;}
+}
